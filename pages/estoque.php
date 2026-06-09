01b9b4c6bde749a7c4d5ebc7c648009ea25b2efd
Hectorpy <victor.lima.nick@gmail.com>
2026-06-09 14:19:24 -0300

    Load database for direct stock page access

--- pages/estoque.php
+++ pages/estoque.php
@@ -1,4 +1,8 @@
 <?php
+if (!isset($pdo) || !$pdo instanceof PDO) {
+    require_once __DIR__ . '/../config/db.php';
+}
+
 require_once __DIR__ . '/../includes/product_schema.php';
 
 ensureProductExtendedSchema($pdo);
